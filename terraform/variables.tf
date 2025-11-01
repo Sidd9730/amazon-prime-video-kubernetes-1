b5a77acfb3170f08ecc1af36c8b4a6e179de9e60
Sidd9730 <siddhantgambhir27@gmail.com>
2025-11-01 22:18:46 +0530

    file corrected

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "instance_name" {
-    default = "Monitoring_server"  # Names of the instance
+  default = "Monitoring_server" # Names of the instance
 }
 
 variable "key_name" {
-  default = "cloudSidd"                  # Names of key in aws
+  default = "primevideo" # Names of key in aws
 }
 
 
 variable "access_key" {
-  default = "insert"                # aws access key
+  default = "insert" # aws access key
 }
 
 variable "secret_key" {
-  default = "insert"         # aws secret key
+  default = "insert" # aws secret key
 }
 
